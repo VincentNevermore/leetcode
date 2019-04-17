b6b07ae621b1621457d4f32f523bb44a4e9349bd
awesome <zhangshengwei950209@gmail.com>
2019-04-17 20:53:15 +0100

    problems0028

---
+++ problems0028.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func strStr(haystack string, needle string) int {
+	len1, len2 := len(haystack), len(needle)
+	for i := 0; i <= len1-len2; i++ {
+		if haystack[i:i+len2] == needle {
+			return i
+		}
+	}
+	return -1
+}
+
+func main() {
+	var result1, result2 int
+	result1 = strStr("hello", "ll")
+	result2 = strStr("aaaaa", "bba")
+	fmt.Println(result1, result2)
+}
