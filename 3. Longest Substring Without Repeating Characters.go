c678ccfc9892a2dc466cdeacbcb5305c47123930
awesome <zhangshengwei950209@gmail.com>
2019-04-18 21:55:10 +0100

    change num of problems

--- 3. Longest Substring Without Repeating Characters.go
+++
@@ -1,9 +0,0 @@
-package main
-
-func lengthOfLongestSubstring(s string) int {
-
-}
-
-func main() {
-	result := lengthOfLongestSubstring("aaaa")
-}
---
+++ problems0004.go
@@ -0,0 +1,10 @@
+package main
+
+func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+    len1, len2 :=len(nums1)
+}
+
+
+func main(){
+
+}
